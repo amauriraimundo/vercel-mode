cffd06e9069e9f7b0645df6cf15213721f50eb0a
Amauri Junior <amauri.dev21@gmail.com>
2023-10-19 17:09:01 -0300

    conectionPool

--- db.js
+++ db.js
@@ -2,11 +2,12 @@
 
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: "consulta_coaf.mysql.dbaas.com.br",
   user: "consulta_coaf",
   password: "Toriba@2023",
   database: "consulta_coaf",
+  connectionLimit: 100,
 });
 
 db.connect((err) => {
